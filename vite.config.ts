7741c323086f2c77dc3da651c6e9ac1cfb94c732
DanilRev04 <danilrev04@gmail.com>
2025-04-25 18:35:38 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
   server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true
+    },
     proxy: {
       '/tasks': {
         target: 'http://backend:3001',
         changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/tasks/, '/tasks'),
       }
-    },
-    host: true,
-    watch: {
-      usePolling: true
     }
   }
-})
+});
